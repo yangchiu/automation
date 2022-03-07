8e08c34ea6d96746f3690995602d2e2577c8ace3
Yang Chiu <yang.chiu@suse.com>
2022-03-07 10:36:56 +0800

    ignore

--- longhorn/port-forwarding.sh
+++
@@ -1,4 +0,0 @@
-kubectl port-forward longhorn-ui-6b965cd5b5-4fj77 30007:8000 -n longhorn-system
-# Forwarding from 127.0.0.1:30007 -> 8000
-# Forwarding from [::1]:30007 -> 8000
-# Then access http://107.20.145.181:30007

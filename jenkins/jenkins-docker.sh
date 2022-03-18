1a7b0e4d705f7349789433138e695934b8b17203
Yang Chiu <yang.chiu@suse.com>
2022-03-18 15:17:33 +0800

    jenkins

---
+++ jenkins/jenkins-docker.sh
@@ -0,0 +1,1 @@
+docker run --name jenkins -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v jenkins_home:/var/jenkins_home jenkins/jenkins
